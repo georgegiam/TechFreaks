74befc70de6c887058664b12d026c0d4c629ca58
nikos kongkika <kongkikan@gmail.com>
2020-07-18 16:37:07 +0300

    Use an action list in index.php to handle all your actions

--- index.php
+++ index.php
@@ -3,5 +3,26 @@
 // Require configuration file
 require_once("config.inc.php");
 
+// Actions list
+$_action = "";
+if(isset($_REQUEST['action']))
+	$_action = $_REQUEST['action'];
+
+switch ($_action) {
+
+	case 'register':
+		require_once("php/register.php");
+		break;
+
+	case 'contact':
+		require_once("php/contact.php");
+		break;
+
+	default:
+		// Do nothing
+		break;
+
+}
+
 // Display content
 echo file_get_contents("views/index.html");
